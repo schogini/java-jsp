0881fb544e579a8770c8934c8e6296fae4701b34
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-26 11:42:37 +0530

    Sree

--- test-8080.sh
+++ test-8080.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-echo $@
-echo "IMAGE: $2 BUILD: $1"
+#echo $@
+#echo "IMAGE: $2 BUILD: $1"
 
 docker network create tmp-net > log.txt 2>&1
 docker container rm -f tmp-web >> log.txt 2>&1
 
 docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2 >> log.txt 2>&1
 
-docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8080/sample/
+#docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8080/sample/
 
 qq=$(docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8080/sample/|grep -c "App Version: $1")
 
@@ -20,3 +20,4 @@ else
 	echo "FAILED" > result.txt
 fi
 cat result.txt
+exit 0
