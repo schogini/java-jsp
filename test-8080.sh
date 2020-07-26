fb02672795b3a5b7808d2eeebde9b120e1e47405
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-26 11:50:44 +0530

    Sree

--- test-8080.sh
+++ test-8080.sh
@@ -8,7 +8,12 @@ docker container rm -f tmp-web >> log.txt 2>&1
 
 docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2 >> log.txt 2>&1
 
-#docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8080/sample/
+# docker ps
+sleep 20
+
+# docker run --rm --net tmp-net curlimages/curl:7.71.1 "http://tmp-web:8080/sample/"
+
+# exit 0
 
 qq=$(docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8080/sample/|grep -c "App Version: $1")
 
