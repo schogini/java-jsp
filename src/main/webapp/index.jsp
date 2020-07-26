edb8d982419598f79bce39c989379edcaae3b209
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-26 11:22:08 +0530

    Sree

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,6 +2,6 @@
 <body>
 <h1>Hello World!</h1>
 <h2>Container ID: <% out.print(System.getenv("HOSTNAME")); %></h2>
-<h2>BUILD#: <% out.print(System.getenv("WEB")); %></h2>
+<h2>App Version: <% out.print(System.getenv("WEB")); %></h2>
 </body>
 </html>
---
+++ test-8080.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker network create tmp-net > log.txt 2>&1
+docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2 >> log.txt 2>&1
+qq=$(docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8080|grep -c "App Version: $1")
+docker container rm -f tmp-web >> log.txt 2>&1
+if [ $qq -gt 0 ]; then
+	echo "SUCCESS" > result.txt
+else
+	echo "FAILED" > result.txt
+fi
+cat result.txt
