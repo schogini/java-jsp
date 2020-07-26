72b7c7a197e19a7e184805efd7b890ca7aa9b7e7
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-26 11:33:01 +0530

    Sree

--- test-8080.sh
+++ test-8080.sh
@@ -2,6 +2,9 @@
 
 docker network create tmp-net > log.txt 2>&1
 docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2 >> log.txt 2>&1
+
+docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8080/sample/
+
 qq=$(docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8080/sample/|grep -c "App Version: $1")
 docker container rm -f tmp-web >> log.txt 2>&1
 if [ $qq -gt 0 ]; then
